510810012022a69fe526eaac54222d5e4663f807
aliu8606 <adotliu34@gmail.com>
2022-03-10 12:29:29 -0500

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args) {
+        String message = "oh hello there";
+        // shift chr 2, horzion shift 5, vertical shift 3
+        String encryMessage = "";
+    }
+}
