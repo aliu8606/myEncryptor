d1b26613da29c5569791dcc4b49573f4171e529b
aliu8606 <adotliu34@gmail.com>
2022-03-11 16:27:42 -0500

    Finished

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -24,8 +24,20 @@ public class Encryptor {
     public String encrypt() {
         encryptCaesar();
         fillBlock(message);
+
         encryptRowShift();
         encryptColShift();
+
+        return readBlock();
+    }
+
+    public String decrypt() {
+        fillBlock(message);
+        decryptColShift();
+        decryptRowShift();
+
+        message = readBlock();
+        decryptCaesar();
         return message;
     }
 
@@ -77,19 +89,6 @@ public class Encryptor {
         message = result;
     }
 
-    private void fillBlock(String str) {
-        int count = 0;
-        for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                if (count < str.length()) {
-                    block[i][j] = str.substring(count, count + 1);
-                    count++;
-                }
-            }
-        }
-
-    }
-
     private void encryptRowShift() {
         int shiftBy = rowShiftKey % numCols;
         for (int i = 0; i < numRows; i++) {
@@ -107,39 +106,127 @@ public class Encryptor {
                 block[i][k] = temp[k];
             }
         }
-        printBlock();
     }
 
     private void encryptColShift() {
-        int shiftBy = colShiftKey % numCols;
-
-        String[][] temp = new String[shiftBy][numRows];
-        int index = 0;
-        for (int j = numCols - shiftBy; j < numCols; j++) {
-            String[] helper = new String[numRows];
-            int helperIndex = 0;
-            for (String[] row : block) {
-                helper[helperIndex] = row[j];
-                helperIndex++;
+        int shiftBy = colShiftKey % numRows;
+        for (int i = 0; i < numCols; i++) {
+            String[] temp = new String[shiftBy];
+            int index = 0;
+            for (int j = numRows - shiftBy; j < numRows; j++) {
+                temp[index] = block[j][i];
+                index++;
             }
-            temp[index] = helper;
-            index++;
-        }
 
-        for (int k = 0; k < temp.length; k++) {
-            if (k < numCols - shiftBy) {
-                for (int l = 0; l < numRows; l++) {
-                    System.out.println(k + " " + block[l][k + shiftBy] + " " + block[l][k]);
-                    block[l][k + shiftBy] = block[l][k];
+            for (int k = 0; k < temp.length; k++) {
+                if (k < numRows - shiftBy) {
+                    block[k + shiftBy][i] = block[k][i];
                 }
-
+                block[k][i] = temp[k];
             }
-            for (int l = 0; l < numRows; l++) {
-                block[l][k] = temp[k][l];
+        }
+    }
+
+    private void decryptCaesar() {
+        String result = message;
+        String clue = caesarKey + "";
+
+        while (clue.length() != result.length()) {
+            if (clue.length() > result.length()) {
+                clue = clue.substring(0, clue.length() - 1);
+            }
+            if (clue.length() < result.length()) {
+                clue += caesarKey;
             }
         }
 
-        printBlock();
+        int j = 0;
+        for (int i = 0; i < result.length(); i++) {
+            char letter = result.charAt(i);
+            int letterInt = (int) letter;
+
+            if (letterInt > 64 && letterInt < 91) {
+                int cue = Integer.parseInt(clue.substring(j, j + 1));
+                j++;
+
+                if (letterInt - cue < 65) {
+                    letter = (char) (letterInt - cue + 26);
+                }
+                else {
+                    letter = (char) (letterInt - cue);
+                }
+            }
+            if (letterInt > 96 && letterInt < 123) {
+                int cue = Integer.parseInt(clue.substring(j, j + 1));
+                j++;
+
+                if (letterInt - cue < 97) {
+                    letter = (char) (letterInt - cue + 26);
+                }
+                else {
+                    letter = (char) (letterInt - cue);
+                }
+            }
+
+            result = result.substring(0, i) + letter + result.substring(i + 1);
+        }
+
+        message = result;
+    }
+
+    private void decryptRowShift() {
+        int shiftBy = rowShiftKey % numCols;
+        for (int i = 0; i < numRows; i++) {
+            String[] temp = new String[shiftBy];
+            int index = 0;
+            for (int j = 0; j < shiftBy; j++) {
+                temp[index] = block[i][j];
+                index++;
+            }
+
+            index = 0;
+            for (int k = shiftBy; k < numCols; k++) {
+                block[i][k - shiftBy] = block[i][k];
+            }
+            for (int l = numCols - shiftBy; l < numCols; l++) {
+                block[i][l] = temp[index];
+                index++;
+            }
+        }
+    }
+
+    private void decryptColShift() {
+        int shiftBy = colShiftKey % numRows;
+        for (int i = 0; i < numCols; i++) {
+            String[] temp = new String[shiftBy];
+            int index = 0;
+            for (int j = 0; j < shiftBy; j++) {
+                temp[index] = block[j][i];
+                index++;
+            }
+
+            index = 0;
+            for (int k = shiftBy; k < numRows; k++) {
+                block[k - shiftBy][i] = block[k][i];
+            }
+            for (int l = numRows - shiftBy; l < numRows; l++) {
+                block[l][i] = temp[index];
+                index++;
+            }
+        }
+    }
+
+    private void fillBlock(String str) {
+        int count = 0;
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if (count < str.length()) {
+                    block[i][j] = str.substring(count, count + 1);
+                    count++;
+                }
+            }
+        }
+
     }
 
     private int[] suggestFactors(int l) {
@@ -151,6 +238,16 @@ public class Encryptor {
         }
 
         int[] result = {possible, l / possible};
+        return result;
+    }
+
+    private String readBlock() {
+        String result = "";
+        for (String[] row : block) {
+            for (String str : row) {
+                result += str;
+            }
+        }
         return result;
     }
 
--- src/Runner.java
+++ src/Runner.java
@@ -1,15 +1,50 @@
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is your message? ");
+        String message = input.nextLine();
+        System.out.print("What is your caesar shift? ");
+        int ca = input.nextInt();
+        System.out.print("What is your row shift? ");
+        int r = input.nextInt();
+        System.out.print("What is your column shift? ");
+        int co = input.nextInt();
+        System.out.print("Encrypt (E) or Decrypt (D)? ");
+        String crypt = input.next();
+
+        Encryptor encryptor = new Encryptor(message, ca, r, co);
+        if (crypt.equals("E")) {
+            System.out.println(encryptor.encrypt());
+        }
+        if (crypt.equals("D")) {
+            System.out.println(encryptor.decrypt());
+        }
+
+        /* test code
         String message = "oh hello there";
         // shift chr 2, horzion shift 5, column shift 3, fill with all
         String caesarm = "qj jgnnq vjgtg";
         String rowshiftm = " jgnnqjvjgtgq ";
-        String colshiftm = "";
-        String encryMessage = "nqj jgngq vjgt";
+        String colshiftm = "vjgtgq  jgnnqj";
+        String encryMessage = colshiftm;
 
         Encryptor encryptor = new Encryptor(message, 2, 5, 3);
-        System.out.println(encryptor.encrypt());
-        System.out.println(encryMessage);
+        //System.out.println(encryptor.encrypt());
+        //System.out.println(encryMessage);
 
+        Encryptor encryptor2 = new Encryptor(encryMessage, 2, 5, 3);
+        //System.out.println(encryptor2.decrypt());
+        //System.out.println(message);
+
+        String mess = "startling";
+        Encryptor encryptor3 = new Encryptor(mess, 4, 2, 8);
+        String mess2 = encryptor3.encrypt();
+
+        Encryptor encryptor4 = new Encryptor(mess2, 4, 2, 8);
+        System.out.println(encryptor4.decrypt());
+        System.out.println(mess);
+        */
     }
 }
